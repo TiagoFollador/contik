ea010fcf6e96227677c1108cafcd084d819324d4
TiagoFollador <tiagobritofollador@gmail.com>
2024-11-06 01:00:23 -0300

    p

---
+++ contik.c
@@ -0,0 +1,92 @@
+#include "contiki.h"
+#include <stdio.h>
+#include <random.h>
+
+PROCESS(batimentoCardiaco, "Batimento Cardiaco");
+PROCESS(oxigenio, "Saturação do Oxigenio");
+PROCESS(batimentoCardiaco, "Batimento Cardiaco");
+
+AUTOSTART_PROCESSES(&batimentoCardiaco, &oxigênio, &batimentoCardiaco);
+
+PROCESS_THREAD(batimentoCardiaco, ev, data)
+{
+	static struct etimer timer;
+
+	PROCESS_BEGIN();
+
+	etimer_set(&timer, CLOCK_SECOND * 3);
+	int batimento = 0;
+	int max = 140;
+	int min = 20;
+ 
+	static char msg[50]
+	while(true) {
+		batimento = random_rand() % (max - min) + min;
+		if (batimento < 50) {
+			sprintf(msg, "Alerta, batimento cardiaco baixo: %d\n", batimento);
+			printf("%s", msg);
+		} else if (batimento > 90) {
+			sprintf(msg, "Alerta, batimento cardiaco alto: %d\n", batimento);
+			printf("%s", msg);
+		} else {
+			sprintf(msg, "Batimento cardiaco normal: %d\n", batimento);
+			printf("%s", msg);
+		}
+	}
+	PROCESS_END();	
+}
+
+PROCESS_THREAD(oxigenio, ev, data)
+{
+	static struct etimer timer;
+
+	PROCESS_BEGIN();
+
+	etimer_set(&timer, CLOCK_SECOND * 3);
+	int oxigenio = 0;
+	int max = 100;
+	int min = 80;
+ 
+	static char msg[50]
+	while(true) {
+		oxigenio = random_rand() % (max - min) + min;
+		if (oxigenio < 90) {
+			sprintf(msg, "Alerta, saturacao de oxigenio baixa: %d%%\n", oxigenio);
+			printf("%s", msg);
+		} else {
+			sprintf(msg, "Saturação do  oxigenio normal: %d%%\n", oxigenio);
+			printf("%s", msg);
+		}
+	}
+	PROCESS_END();
+}
+
+PROCESS_THREAD(oxigenio, ev, data)
+{
+	static struct etimer timer;
+
+	PROCESS_BEGIN();
+
+	etimer_set(&timer, CLOCK_SECOND * 3);
+	int temperatura = 0;
+	int max = 41;
+	int min = 34;
+ 
+	static char msg[50]
+	while(true) {
+		temperatura = random_rand() % (max - min) + min;
+		if (temperatura < 35) {
+			sprintf(msg, "Alerta, Hipotermia: %d\n", temperatura);
+			printf("%s", msg);
+		} else if (temperatura > 90) {
+			sprintf(msg, "Alerta, Febre: %d\n", temperatura);
+			printf("%s", msg);
+		} else {
+			sprintf(msg, "Temperatura normal: %d\n", temperatura);
+			printf("%s", msg);
+		}
+		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+		etimer_reset(&timer); 
+	}
+	PROCESS_END();
+}
