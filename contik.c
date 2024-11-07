650d953dd829779bc2a4473308715024de12e0ba
TiagoFollador <tiagobritofollador@gmail.com>
2024-11-07 02:06:42 -0300

    alteracao

--- contik.c
+++ contik.c
@@ -2,14 +2,21 @@
 #include <stdio.h>
 #include <random.h>
 
-#define MIN_VAL 20
-#define MAX_VAL 140
+#define BATIMENTO_MIN 20
+#define BATIMENTO_MAX 140
+
+#define OXIGENIO_MAX 100;
+#define OXIGENIO_MIN 80;
+
+#define TEMPERATURA_MAX 41;
+#define TEMPERATURA_MIN 34;
 
 PROCESS(batimentoCardiaco, "Batimento Cardiaco");
 PROCESS(oxigenio, "Saturação do Oxigenio");
 PROCESS(temperatura, "Temperatura Corporal");
+PROCESS(evento, "Evento");
 
-AUTOSTART_PROCESSES(&batimentoCardiaco, &oxigenio, &temperatura);
+AUTOSTART_PROCESSES(&batimentoCardiaco, &oxigenio, &temperatura, &evento);
 
 PROCESS_THREAD(batimentoCardiaco, ev, data)
 {
@@ -23,13 +30,15 @@ PROCESS_THREAD(batimentoCardiaco, ev, data)
 
     while(true) {
 
-        batimento =   random_rand() % (MAX_VAL - MIN_VAL) + MIN_VAL;
+        batimento =   random_rand() % (BATIMENTO_MIN - BATIMENTO_MAX) + BATIMENTO_MAX;
         if (batimento < 50) {
             sprintf(msg, "Alerta, batimento cardiaco baixo: %d\n", batimento);
             printf("%s", msg);
+            process_post(&gerenciadorDeEventos, PROCESS_EVENT_MSG, (void *)"BATIMENTO_BAIXO");
         } else if (batimento > 90) {
             sprintf(msg, "Alerta, batimento cardiaco alto: %d\n", batimento);
             printf("%s", msg);
+            process_post(&gerenciadorDeEventos, PROCESS_EVENT_MSG, (void *)"BATIMENTO_ALTO");
         } else {
             sprintf(msg, "Batimento cardiaco normal: %d\n", batimento);
             printf("%s", msg);
@@ -49,16 +58,16 @@ PROCESS_THREAD(oxigenio, ev, data)
 
     etimer_set(&timer, CLOCK_SECOND * 3);
     int oxigenio = 0;
-    int max = 100;
-    int min = 80;
+
     static char msg[50];
 
     while(true) {
 
-        oxigenio =   random_rand() % (max - min) + min;
+        oxigenio =   random_rand() % (OXIGENIO_MAX - OXIGENIO_MIN) + OXIGENIO_MIN;
         if (oxigenio < 90) {
             sprintf(msg, "Alerta, saturacao de oxigenio baixa: %d%%\n", oxigenio);
             printf("%s", msg);
+            process_post(&gerenciadorDeEventos, PROCESS_EVENT_MSG, (void *)"OXIGENIO_BAIXO");
         } else {
             sprintf(msg, "Saturação do oxigenio normal: %d%%\n", oxigenio);
             printf("%s", msg);
@@ -78,19 +87,20 @@ PROCESS_THREAD(temperatura, ev, data)
 
     etimer_set(&timer, CLOCK_SECOND * 3);
     int temperatura = 0;
-    int max = 41;
-    int min = 34;
+
     static char msg[50];
 
     while(true) {
 
-        temperatura =   random_rand() % (max - min) + min; 
+        temperatura =   random_rand() % (TEMPERATURA_MAX - TEMPERATURA_MIN) + TEMPERATURA_MIN; 
         if (temperatura < 35) {
-            sprintf(msg, "Alerta, Hipotermia: %d\n", temperatura);
+            sprintf(msg, "Alerta, Hipotermia: %d\n", temperatura); 
             printf("%s", msg);
+            process_post(&gerenciadorDeEventos, PROCESS_EVENT_MSG, (void *) "HIPOTERMIA");
         } else if (temperatura > 39) {
             sprintf(msg, "Alerta, Febre: %d\n", temperatura);
             printf("%s", msg);
+            process_post(&gerenciadorDeEventos, PROCESS_EVENT_MSG, (void *) "FEBRE");
         } else {
             sprintf(msg, "Temperatura normal: %d\n", temperatura);
             printf("%s", msg);
@@ -98,6 +108,33 @@ PROCESS_THREAD(temperatura, ev, data)
 
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
         etimer_reset(&timer);
+    }
+
+    PROCESS_END();
+}
+
+
+PROCESS_THREAD(evento, ev, data)
+{
+    PROCESS_BEGIN();
+
+    while(true) {
+        PROCESS_WAIT_EVENT();
+        if (ev == PROCESS_EVENT_MSG) {
+            char *evento = (char *)data;
+
+            if (evento == "BATIMENTO_BAIXO") {
+                printf("ALERTA: Batimento cardíaco abaixo do normal!\n");
+            } else if (evento == "BATIMENTO_ALTO") {
+                printf("ALERTA: Batimento cardíaco acima do normal!\n");
+            } else if (evento == "OXIGENIO_BAIXO") {
+                printf("ALERTA: Saturação de oxigênio abaixo do normal!\n");
+            } else if (evento == "HIPOTERMIA") {
+                printf("ALERTA: Temperatura corporal abaixo do normal!\n");
+            } else if (evento == "FEBRE") {
+                printf("ALERTA: Temperatura corporal acima do normal!\n");
+            }
+        }
     }
 
     PROCESS_END();
